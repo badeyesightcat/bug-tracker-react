76e3e96bafa8284f23ba6a0f3286e41f3ef9a94d
Beom Mee <whitebigbabybird@gmail.com>
2023-02-04 11:59:52 +0900

    Add login page and its stylesheet

---
+++ frontend/src/Views/Login/login.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { signIn } from '../../Controllers/Redux/authSlice';
+import './login.css';
+
+export default () => {
+  const dispatch = useDispatch();
+  const [formField, setFormField] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleFormFieldChange = (e) => {
+    setFormField((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(signIn(formField));
+  };
+
+  return (
+    <section className='login-wrap flex justify-center items-center'>
+      <form action='' className='login-form flex flex-col'>
+        <fieldset className='flex flex-col login-input-section'>
+          <legend className='brandname text-center mb-8'>Landscape</legend>
+          <input
+            type='text'
+            name='username'
+            placeholder='username'
+            onChange={handleFormFieldChange}
+            value={formField.name}
+            className='login-input'
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='password'
+            onChange={handleFormFieldChange}
+            value={formField.password}
+            className='login-input'
+          />
+        </fieldset>
+        <button type='submit' onClick={handleSubmit} className='login-btn'>
+          Log In
+        </button>
+      </form>
+    </section>
+  );
+};
