0ba365fb846f32a3925e20b7d7750a5829a6d40b
Beom Mee <whitebigbabybird@gmail.com>
2023-02-05 20:48:12 +0900

    Rename bugs slice and modify getBugs mutation to fix error

--- frontend/src/Controllers/Redux/bugSlice.js
+++ frontend/src/Controllers/Redux/bugSlice.js
@@ -2,12 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 import { retrieveBugs } from '../bugController';
 
 const slice = createSlice({
-  name: 'bug',
+  name: 'bugs',
   initialState: [],
   reducers: {
     getBugs: (state) => {
-      // state = retrieveBugs();
-      retrieveBugs();
+      state.push(...retrieveBugs());
     },
     createBug: (state, action) => {},
     updateBug: (state, action) => {},
