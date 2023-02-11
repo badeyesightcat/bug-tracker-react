54c3d2380166b4c187dfdc363687ffbd2c60e16d
Beom Mee <whitebigbabybird@gmail.com>
2023-02-11 12:56:00 +0900

    Change route name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Views/pages/Login';
-import ViewBugs from './Views/pages/BugsList';
+import BugsList from './Views/pages/BugsList';
 import Sidebar from './Views/components/Sidebar';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <>
           <Sidebar />
           <Routes>
-            <Route path='/viewBugs' element={<ViewBugs />}></Route>
+            <Route path='/bugsList' element={<BugsList />}></Route>
           </Routes>
         </>
       )}
