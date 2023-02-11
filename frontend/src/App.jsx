a59a64ff652503cf7c12ec3056ca836b70bb275b
Beom Mee <whitebigbabybird@gmail.com>
2023-02-11 11:59:16 +0900

    Add router functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Views/Login/login';
 import ViewBugs from './Views/Pages/viewBugs';
 import Sidebar from './Views/Sidebar/sidebar';
@@ -14,7 +14,9 @@ function App() {
       ) : (
         <>
           <Sidebar />
-          <ViewBugs />
+          <Routes>
+            <Route path='/viewBugs' element={<ViewBugs />}></Route>
+          </Routes>
         </>
       )}
     </Router>
