6416036d74c99d1a7eef7d15ade00e50ec4ea2a9
Beom Mee <whitebigbabybird@gmail.com>
2023-02-14 15:53:47 +0900

    Change markup structure of the detailed bugs view

--- frontend/src/Views/components/bugView/index.jsx
+++ frontend/src/Views/components/bugView/index.jsx
@@ -20,24 +20,31 @@ export default function BugView(props) {
   return (
     <div className='drawer-container'>
       <div className='backdrop'></div>
-      <div className='drawer has-bug-details'>
-        <button
-          type='button'
-          title='Close the detail view'
-          className='btn-close-drawer'
-          onClick={handleClickClose}>
-          ✕
-        </button>
-        <h3 className='bug-detail-title'>{title}</h3>
-        <p className='bug-detail-explanation'>{details}</p>
-        <div className='bug-detail-meta'>
-          <span>creator: {creator}</span>
-          <span>created on: {time}</span>
+      <div className='drawer has-bug-details grid grid-cols-3 grid-rows-1'>
+        <div className='col-span-2'>
+          <button
+            type='button'
+            title='Close the detail view'
+            className='btn-close-drawer'
+            onClick={handleClickClose}>
+            ✕
+          </button>
+          <h3 className='bug-detail-title'>{title}</h3>
+          <p className='bug-detail-explanation'>{details}</p>
         </div>
-        <div className='bug-detail-meta'>
-          <span>priority: {priority}</span>
-          <span>assigned to: {assigned}</span>
-          <span>version: {version}</span>
+        <div className='columns-1 flex flex-col gap-8'>
+          <div className='bug-detail-meta'>
+            <span className='bug-detail-meta-item'>creator: {creator}</span>
+            <span className='bug-detail-meta-item'>created on: {time}</span>
+            <span className='bug-detail-meta-item'>priority: {priority}</span>
+            <span className='bug-detail-meta-item'>
+              assigned to: {assigned}
+            </span>
+            <span className='bug-detail-meta-item'>version: {version}</span>
+          </div>
+          <button type='button' className='btn-mark-complete'>
+            Mark complete
+          </button>
         </div>
       </div>
     </div>
